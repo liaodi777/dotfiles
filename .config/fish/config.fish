5a9ea98b24659a5a4171a7dd49502efc56a3ce41
liaodi777 <102370681+liaodi777@users.noreply.github.com>
2024-01-31 13:18:01 +0900

    add peco & ghq setting on fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# peco
+function fish_user_key_bindings
+  bind \cr peco_select_history # Bind for peco select history to Ctrl+R
+  bind \cf peco_change_directory # Bind for peco change directory to Ctrl+F
+end
+
 starship init fish | source
 ~/.local/bin/mise activate fish | source
